f7eb685bc2f09cc6f577b4cd594ce0d450bcd69e
Alanna <20211064010001@ifrn.local>
2024-12-16 17:21:35 -0300

    Projeto Pronto

---
+++ Atividade/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Alanna", 20);
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+
+	}
+
+}
